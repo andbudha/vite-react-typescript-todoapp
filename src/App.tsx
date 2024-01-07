e4284a8709c69cd7c81aff6fda5cd69ef933b39f
andbudha <andreibartov@gmail.com>
2024-01-07 12:51:49 +0100

    app update

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className={'main-container'}>
+      <div className={'app-box container md'}>
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
