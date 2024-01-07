4972118b400e95516713b7d6af9b07f93647a90b
andbudha <andreibartov@gmail.com>
2024-01-07 15:17:47 +0100

    Todolist updated and styled

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,8 +1,20 @@
+import { useSelector } from 'react-redux';
+import { AppRootState } from '../../redux/store';
+import { Todo } from '../../assets/types/types';
+import { TodoItem } from '../Todo/TodoItem';
+import { Input } from '../Input/Input';
+
 type TodoList = {};
-export const TodoList = (props: TodoList) => {
+export const TodoList = () => {
+  const todoList = useSelector<AppRootState, Todo[]>((state) => state.todolist);
+  const list = todoList.map((todo) => {
+    return <TodoItem key={todo.id} title={todo.title} status={todo.status} />;
+  });
   return (
-    <div>
-      <h2>TodoList</h2>
+    <div className="container sm flex flex-col justify-center items-center">
+      <h2>Todo List</h2>
+      <Input />
+      {list}
     </div>
   );
 };
